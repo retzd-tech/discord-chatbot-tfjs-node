be3fd3d222382baf38eb6864bfa177a1d44944f9
aland.tiwa <aland.tiwa@btpn.com>
2021-01-19 18:27:23 +0700

    add new constant for nexus log

--- constant/index.js
+++ constant/index.js
@@ -12,6 +12,7 @@ const Constant = {
   },
   READ_FILE_STANDARD: 'utf-8',
   CHECK_IN_LOG_DIRECTORY: './data/checkIn.log',
+  CHECK_NEXUS_LOG_DIRECTORY: './data/nexusLog.log',
   DATA_DIRECTORY: './data/',
   CHECK_IN_MEMBERS_DIRECTORY: './data/checkedInMembers.log',
   BOT_KNOWLEDGE: './data/bot_knowledge.txt'
