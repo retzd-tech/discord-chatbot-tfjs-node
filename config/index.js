a1cb67dd78c4f81cfa6781bac540b18d1dad8d28
alandsamuel <aland_samuel@yahoo.com>
2021-02-20 17:07:34 +0700

    update config

--- config/index.js
+++ config/index.js
@@ -2,6 +2,5 @@ require("dotenv").config();
 
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
-  USERNAME: process.env.USERNAME,
-  PASSWORD: process.env.PASSWORD,
+  CREDENTIALS: process.env.CREDENTIALS
 };
